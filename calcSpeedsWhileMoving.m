546296df576b26a470481799ac95d1ad45c55ca4
mbinazhar <abdullah22@gmail.com>
2017-12-13 15:19:39 +0500

    coarse obstacle avoidance

--- calcSpeedsWhileMoving.m
+++ calcSpeedsWhileMoving.m
@@ -1,6 +1,5 @@
-function [vLeft,vRight] = calcSpeedsWhileMoving( i,x,y,theta,x_g,y_g )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+function [vLeft,vRight,hasReached] = calcSpeedsWhileMoving( i,x,y,theta,x_g,y_g )
+% SET TURN FLAGS TO ZERO FOR TURN-WHILE-MOVING and 1 for TURN-THEN-MOVE
 Kp =32;%12;
 Ki =12;%5;
 Kd =8;%5
@@ -14,10 +13,12 @@ defaultSpeed = 80;
 
 persistent E_k;
 persistent e_k_1;
+persistent turnFlags;
 
 if(isempty(E_k))
     E_k    = [0;0;0];
     e_k_1 = [0;0;0];
+    turnFlags = [1;1;1];
 end
 
 
@@ -42,15 +43,34 @@ w = Kp*e_P + Ki*e_I + Kd*e_D;
 E_k(i) = e_I;
 e_k_1(i) = e_k;
 
+vRot = w;
+hasReached=0;
 
-if (dist>distThresh) % && (abs(e_k)>0.2) 
-    vRot = w;
+
+turnFlags(i)=0; % ************** DANGER ***************
+if abs (e_k)  > deg2rad(5) && turnFlags(i) == 1 && dist > distThresh
+%     vLeft =  -e_k/abs(e_k)*50;
+%     vRight = +e_k/abs(e_k)*50;
+    vLeft =  -vRot;
+    vRight = +vRot;
+    
+elseif (dist > distThresh)
+    if turnFlags(i)==1
+        turnFlags(i)=0;
+        E_k(i)=0;e_k_1(i)=0;
+        vLeft=0;
+        vRight=0;
+    else
+        E_k(i)=0; % ******************** DANGER ************ TO AVOID BALANCING OF SENSOR REPULSION AND ORIENTATING TOWARDS GOAL
     vLeft =  defaultSpeed-vRot;
     vRight = defaultSpeed+vRot;
+    end
+    
 else
     vLeft=0;
     vRight=0;
-    E_k(i)=0;
+    turnFlags(i)=1;
+    hasReached=1;
 end
 
 if( vLeft > 300)
@@ -66,19 +86,19 @@ if( vRight < -300)
     vRight = -300;
 end
 
-if abs(vLeft) < 10
-    vLeft=0;
-end
-if abs(vRight) < 10
-    vRight=0;
-end
+% if abs(vLeft) < 10
+%     vLeft=0;
+% end
+% if abs(vRight) < 10
+%     vRight=0;
+% end
 
 % if vLeft == NaN || vRight == NaN
 %     vLeft=0;vRight=0;
 % end
 
 
-vLeft=round(vLeft);
-vRight=round(vRight);
+% vLeft=round(vLeft);
+% vRight=round(vRight);
 end
 
--- mubassirSubscriber.m
+++ mubassirSubscriber.m
@@ -1,20 +1,23 @@
 function mubassirSubscriber(~, message)
     %mubassirSubscriber Subscriber callback function for pose data    
     %   mubassirSubscriber(~,MESSAGE) returns data of detected markers
-    %   n = number of marekers
-    %   markers, vector of marker pose data
-
-    %pos = [message.Linear.X message.Linear.Y message.Linear.Z];
-    %orient = [message.Angular.X message.Angular.Y message.Angular.Z];
-    global robots
+    global zumoPose
+    global robotIds
     n = size(message.Poses);
-    %robots = zeros(n,4);
-    for z = 1 : n(1,1)
-        id = message.Poses(z).Orientation.X;
-        x = message.Poses(z).Position.X;
-        y = message.Poses(z).Position.Y;
-        theta = message.Poses(z).Orientation.Y;
-        robots(z,:) = [id x y theta];
+    for i = 1 : n(1,1)
+        id = message.Poses(i).Orientation.X;
+        x =  message.Poses(i).Position.X;
+        y = -message.Poses(i).Position.Y;
+        % - minus added to confirm with ROS Co-ordinate
+        % x front, y left, theta = 0 in positive x direction, counter
+        % clockwise increase, remains within -pi and pi
+        theta = message.Poses(i).Orientation.Y;
+        if id==robotIds(1,1)
+            zumoPose(1,:) = [x y theta];
+        elseif id==robotIds(1,2)
+            zumoPose(2,:) = [x y theta];
+        elseif id==robotIds(1,3)
+            zumoPose(3,:) = [x y theta];
+        end
     end
-    robots;
 end
